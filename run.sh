bb59cbe8de09381d70aa76be9d8784ba2d3e785d
billsjchw <billsjc@126.com>
2021-01-10 06:03:46 -0500

    feat: implement basic simulation

---
+++ run.sh
@@ -0,0 +1,3 @@
+mkdir -p logs
+python3 init_redis.py
+python3 simulate.py
---
+++ simulate.py
@@ -0,0 +1,104 @@
+import grpc
+import guide_pb2
+import guide_pb2_grpc
+import multiprocessing
+import json
+import time
+import random
+import datetime
+
+OFFSETS = [(0, 0), (0, -1), (0, 1), (1, 0), (-1, 0)]
+
+
+class Car:
+    def __init__(self, car_id, src_pos, dst_pos, t_avg, t_std):
+        self.car_id = car_id
+        self.seq = 0
+        self.src_pos = src_pos
+        self.dst_pos = dst_pos
+        self.cur_pos = src_pos
+        self.last_pos = src_pos
+        self.t_avg = t_avg
+        self.t_std = t_std
+
+    def move(self, step_code):
+        time.sleep(random.gauss(self.t_avg, self.t_std))
+        self.seq += 1
+        self.last_pos = self.cur_pos
+        offset = OFFSETS[step_code]
+        self.cur_pos = (self.cur_pos[0] + offset[0], self.cur_pos[1] + offset[1])
+        if self.cur_pos == self.dst_pos:
+            self.src_pos, self.dst_pos = self.dst_pos, self.src_pos
+
+
+class ClientProcess(multiprocessing.Process):
+    def __init__(self, car, server_addrs, log_filename):
+        super(ClientProcess, self).__init__()
+        self.car = car
+        self.server_addrs = server_addrs
+        self.log_filename = log_filename
+
+    def run(self):
+        log_file = open(self.log_filename, 'w')
+
+        def log(msg_type, msg):
+            log_file.write(f'[{msg_type}] {datetime.datetime.now()} {msg}\n')
+            log_file.flush()
+
+        def make_stub(server_addr):
+            channel = grpc.insecure_channel(f"{server_addr['host']}:{server_addr['port']}")
+            return guide_pb2_grpc.GuideStub(channel)
+
+        def make_car_state(car):
+            return guide_pb2.CarState(
+                car_id=car.car_id,
+                seq=car.seq,
+                cur_pos=guide_pb2.CarState.Position(
+                    row_idx=car.cur_pos[0],
+                    col_idx=car.cur_pos[1],
+                ),
+                last_pos=guide_pb2.CarState.Position(
+                    row_idx=car.last_pos[0],
+                    col_idx=car.last_pos[1],
+                ),
+                dst_pos=guide_pb2.CarState.Position(
+                    row_idx=car.dst_pos[0],
+                    col_idx=car.dst_pos[1],
+                ),
+            )
+
+        def get_next_step(stub, car_state):
+            while True:
+                try:
+                    return stub.GetNextStep(car_state)
+                except grpc.RpcError as err:
+                    log('Error', err.details())
+
+        server_addr = random.choice(self.server_addrs)
+        stub = make_stub(server_addr)
+        while True:
+            log('Info', f'car_id={self.car.car_id}, pos={self.car.cur_pos}')
+            car_state = make_car_state(self.car)
+            step = get_next_step(stub, car_state)
+            car.move(step.step_code)
+
+
+if __name__ == '__main__':
+    config = json.load(open('config.json', 'r'))
+    
+    client_processes = []
+    for car_id, car_prop in enumerate(config['car_props']):
+        car = Car(
+            car_id,
+            tuple(car_prop['src_pos']),
+            tuple(car_prop['dst_pos']),
+            config['t_avg'],
+            config['t_std'],
+        )
+        client_process = ClientProcess(car, config['server_addrs'], f'logs/client{car_id}.log')
+        client_processes.append(client_process)
+        client_process.start()
+    
+    time.sleep(config['duration'])
+    for client_process in client_processes:
+        client_process.kill()
